b7e766ea6e7c7decb907c4b82448a0013d1931c2
Michael Anderson <manderson3568@outlook.com.au>
2022-12-30 16:32:48 +1100

    Few adjustments before testing redeploy

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,12 +1,15 @@
 import ColouredCircles from "../components/ColouredCircles";
+import HomeBlogs from "../components/HomeBlogs/HomeBlogs";
 
 function Blogs() {
   return (
     <div>
-      <h1>
+      {/* <h1>
         <ColouredCircles />
         Blogs
-      </h1>
+      </h1> */}
+      <HomeBlogs />
+      <p style={{ textAling: "center" }}>More Blogs coming soon.... </p>
     </div>
   );
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,12 +1,15 @@
 import ColouredCircles from "../components/ColouredCircles";
+import HomeProjects from "../components/HomeProjects/HomeProjects";
 
 function Projects() {
   return (
     <div>
-      <h1>
+      {/* <h1>
         <ColouredCircles />
         Projects
-      </h1>
+      </h1> */}
+      <HomeProjects />
+      <p style={{ textAling: "center" }}>More projects coming soon... </p>
     </div>
   );
 }
